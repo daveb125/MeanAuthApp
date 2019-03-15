17e8a8ca35cc2adaa5c4c268e74ad2aebf3ec82c
Jeremy Singh <jeremy.singh@fullstack.co.za>
2019-03-15 11:28:54 +0200

    added form validation for Register

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  name : string ;
+  username : string;
+  email : string;
+  password: string;
+
+  constructor(private validateService : ValidateService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    console.log(this.name);
+    const user = {
+      name : this.name,
+      username: this.username,
+      email : this.email,
+      password : this.password
+    }
+
+    //Required Fields
+    if (!this.validateService.validateRegister(user))
+    {
+      console.log('Failed');
+    }
+    else
+    {
+
+    }
   }
 
 }
